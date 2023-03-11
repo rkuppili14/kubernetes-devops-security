b243b06bf1f76d8b9472ff8cac471a229b74e12a
rkuppili14 <ravishankar.kuppili@gmail.com>
2023-03-11 11:30:55 +0530

    pit test kill

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -50,7 +50,7 @@ public class NumericApplicationTests {
 	@Test
     public void welcomeMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
-                //.andExpect(content().string("Welcome to DevOps World!!"));
+                .andExpect(content().string("Welcome to DevOps World!!"));
     }
 
 }
